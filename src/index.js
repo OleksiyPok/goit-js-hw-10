9a7ad8d8e1065b8b02efbe296e95afab4b1ca0e9
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2023-02-27 13:01:08 +0100

    fin_00

--- src/index.js
+++ src/index.js
@@ -18,20 +18,18 @@ function onInput(e) {
   const countryName = e.target.value.trim();
   const inputLength = countryName.length;
 
-  countriesListContainer.innerHTML = '';
-  countryInfoContainer.innerHTML = '';
-
   if (inputLength) fetchCountries(countryName);
 }
 
 function fetchCountries(name) {
-  // console.log(URL + name + `?fields=${OPT.join(',')}`);
   fetch(URL + name + `?fields=${OPT.join(',')}`)
     .then(responce => {
       return responce.json();
     })
     .then(countriesData => {
-      console.log('countriesData.length:', countriesData.length);
+      countriesListContainer.innerHTML = '';
+      countryInfoContainer.innerHTML = '';
+      // console.log('countriesData.length:', countriesData.length);
       if (countriesData.length > 10) {
         Notify.info(
           'Too many matches found. Please enter a more specific name.',
@@ -70,17 +68,38 @@ function drawCountryList(countriesData) {
 }
 
 function drawCountryInfo([countryData]) {
-  // const { [countryData.flags.svg: countryFlag ]} = countryData;
-  const countryItem = `
-    <img  class="country-info__img" width="30px"
-                  src="${countryData.flags.svg}"
-                  alt="${countryData.name.official} flag">
-    <span class="country-info__name">${countryData.name.official}</span>
-    <p class="country-info__capital">${countryData.capital}</p>
-    <p class="country-info__population">${countryData.population}</p>
-    <p class="country-info__languages">${Object.values(
-      countryData.languages
-    ).join(', ')}</p>
+  const {
+    flags: { svg: flagSvg },
+    name: { official: officialName },
+    capital,
+    population,
+    languages,
+  } = countryData;
+
+  const countryInfo = `
+    <div class="country-info__title">
+      <img  class="country-info__img" width="30px"
+        src="${flagSvg}"
+        alt="${officialName} flag">
+      </img>
+      <span class="country-info__name">    
+        <b> ${officialName}</b>
+      </span>
+    </div>
+    <ul class="country-info__list">
+      <li class="country-info__capital">
+        <b>Capital: </b>
+          ${capital}
+      </li>
+      <li class="country-info__population">
+        <b>Population: </b>
+          ${population}
+      </li>
+      <li class="country-info__languages">
+        <b>Languages: </b>
+          ${Object.values(languages).join(', ')}
+      </li>
+    </ul>
   `;
-  countryInfoContainer.innerHTML = countryItem;
+  countryInfoContainer.innerHTML = countryInfo;
 }
